d07973acb9cee9e9fe5536771308dc8021e49eba
MrxMilzu <89538890+MrxMilzu@users.noreply.github.com>
2022-08-24 16:03:51 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,411 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.thum = fs.readFileSync("./media/zaki.jpg")
+
+global.autoReadAll = true
+// Ganti Sewajarnya
+global.owner = ['6283182713104']
+global.pengguna = 'Milzu'
+global.premium = ['6283182713104']
+global.author = 'Milzu-TC'
+global.pemilik = ['6285878313791'] 
+global.ownernomer = '6283182713104'
+global.ownername = 'Milzu'
+global.botname = '❑ Milzu-TC'
+global.Prefix = 'Multi Prefix'
+global.meki = '6283182713104'
+global.footer = '❑ List Menu.'
+global.footer1 = '❑ Thanks To \n ❑ Milzu \n ❑ Xinzuo \n AryBotz \n ❑ Gojo Satoru \n ❑ Anggota Xinzuo-MD'
+global.footer2 = 'Jika Menemukan Bug Lapor Ke Polisi Karena Itu Bukan Urusan Gua'
+global.yt = ''
+global.ig = 'https://instagram.com/milzurl_90'
+global.gc = ''
+global.dana = '083182423040'
+global.gopay = '083182713104 \n\n Mau sewa Bot Ketik .Owner'
+global.wame = 'wa.me/6283182713104'
+global.email = 'yaztex771@gmail.com'
+global.region = 'Indonesia'
+global.link = 'nekopoi.care'
+global.sc = 'https://bit.ly/SubscribeSxce Gota'
+global.packname = 'Au : Milzu-TC \n YT : Sxce Gota \n IG : milzurl_90 \n Fb : Mxtquest'
+global.author = '❑ Milzu-TC ❑'
+global.sessionName = 'session'
+global.prefa = ['#','!','/','']
+global.sp = '❑ ' 
+global.mess = {
+    success: '*Ada Perlu Apa ya Kak?* \n *Kok <P,P,P> In Saya* 🗿',
+    admin: '*Fitur hanya bisa digunakan oleh admin grup!*',
+    botAdmin: '*Jadikan Botnya Admin Dulu ;v*',
+    owner: 'Fitur hanya bisa digunakan oleh Pembuat Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: '*Sabar...* \n *Ya Ngab...* \n *Jangan Spam Ashu...* \n\n *Utamakan Jangan Spam...* \n *Kalo Lu Spam Mati Bot Gue !!* \n Gue Gedik Lu Sampe Ke Neraka Anj -_-*',
+    error: '*Fiturnya eror...*',
+        endLimit: '*Ya Elah Limit Lu Abis Ashu. Beli dulu Limitnya Ke Owner Bot Atau Tunggu Aja Sampe Jam 12 akan Disetel ulang*',
+    wrongFormat: 'Perintah Salah!!\nSertakan Link setelah Command..',
+    example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'
+,
+    example2: 'Sampai Jumpa @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'    
+    
+}
+    global.limitawal = {
+    premium: "10",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./media/zaki.jpg')
+global.fakeImg = fs.readFileSync('./media/fake.jpg')
+
+//Ambil dr chika
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//Menu
+global.ccomannya = `
+
+a⃠ *A͎u͎*      : *M͎i͎l͎z͎u͎*
+a⃠ *Y͎T͎*      : *S͎x͎c͎e͎ G͎o͎t͎a͎*
+a⃠ *F͎B͎*      : *M͎x͎t͎ Q͎u͎e͎s͎t͎*
+a⃠ *I͎G͎*       : *m͎i͎l͎z͎u͎r͎l͎_90*
+a⃠ *N͎o͎*      : *6283182713104*
+
+a⃠ *S͎c͎r͎i͎p͎t͎ B͎y͎ M͎i͎l͎z͎u͎ D͎o͎n͎'t͎ S͎p͎a͎m͎/C͎a͎l͎l͎ M͎e͎!!*
+
+a⃠ *J͎i͎k͎a͎ A͎n͎d͎a͎ M͎e͎n͎e͎l͎p͎o͎n͎ B͎o͎t͎ O͎t͎o͎m͎a͎t͎i͎s͎ B͎l͎o͎c͎k͎!!*
+
+
+  「 *A͎l͎l͎ M͎e͎n͎u͎* 」
+
+ . sᴘᴇᴇᴅᴛᴇsᴛ
+ . ᴘɪɴɢ
+ . ᴏᴡɴᴇʀ
+ . ᴍᴇɴᴜ
+ . ᴅᴇʟᴇᴛᴇ
+ . ɪɴғᴏᴄʜᴀᴛ
+ . ǫᴜᴏᴛᴇᴅ
+ . ʟɪsᴛᴘᴄ
+ . ʟɪsᴛᴏɴʟɪɴᴇ 
+  
+  「 *G͎r͎o͎u͎p͎ m͎e͎n͎u͎* 」
+ . ʟɪɴᴋɢʀᴏᴜᴘ
+ . ᴇᴘʜᴇᴍᴇʀᴀʟ [ᴏᴘᴛɪᴏɴ]
+ . sᴇᴛᴘᴘɢᴄ [ɪᴍᴀɢᴇ]
+ . sᴇᴛɴᴀᴍᴇ [ᴛᴇxᴛ]
+ . sᴇᴛᴅᴇsᴄ [ᴛᴇxᴛ]
+ . ɢʀᴏᴜᴘ [ᴏᴘᴛɪᴏɴ]
+ . ᴇᴅɪᴛɪɴғᴏ [ᴏᴘᴛɪᴏɴ]
+ . ᴀᴅᴅ @user
+ . ᴋɪᴄᴋ @user
+ . ʜɪᴅᴇᴛᴀɢ [ᴛᴇxᴛ]
+ . ᴛᴀɢᴀʟʟ [ᴛᴇxᴛ]
+ . ᴀɴᴛɪʟɪɴᴋ [ᴏɴ/ᴏғғ]
+ . ᴍᴜᴛᴇ [ᴏɴ/ᴏғғ]
+ . ᴘʀᴏᴍᴏᴛᴇ @user
+ . ᴅᴇᴍᴏᴛᴇ @user
+ . ᴠᴏᴛᴇ [ᴛᴇxᴛ]
+ . ᴅᴇᴠᴏᴛᴇ
+ . ᴜᴘᴠᴏᴛᴇ
+ . ᴄᴇᴋᴠᴏᴛᴇ
+ . ʜᴀᴘᴜsᴠᴏᴛᴇ
+
+「 *O͎w͎n͎e͎r͎ M͎e͎n͎u͎* 」
+ . ʀᴇᴀᴄᴛ [ᴇᴍᴏᴊɪ]
+ . ᴄʜᴀᴛ [ᴏᴘᴛɪᴏɴ]
+ . ᴊᴏɪɴ [ʟɪɴᴋ]
+ . ʟᴇᴀᴠᴇ 
+ . ʙʟᴏᴄᴋ @user
+ . sᴇᴛʙɪᴏʙᴏᴛ [ᴛᴇxᴛ]
+ . ᴜɴʙʟᴏᴄᴋ @user
+ . ʙᴄɢʀᴏᴜᴘ [ᴛᴇxᴛ]
+ . ʙᴄᴀʟʟ [ᴛᴇxᴛ]
+ . sᴇᴛᴘᴘʙᴏᴛ [ɪᴍᴀɢᴇ]
+ . sᴇᴛᴇxɪғ
+
+  「 *Rpg Menu* 」
+ . ʙᴇʀʙᴜʀᴜ
+ . ᴍᴇᴍᴀɴᴄɪɴɢ
+ . ᴍᴇɴᴀᴍʙᴀɴɢ
+ . ᴍɪɴɪɴɢ
+ . ʜᴇᴀʟ
+ . ᴘʀᴏғɪʟ
+ . ɪɴᴠᴇɴᴛᴏʀɪ
+ . ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ
+ . ʙᴜʏ
+ . sᴇʟʟ
+
+  「 *Downloader Menu* 」
+ . ᴛɪᴋᴛᴏᴋ [ᴜʀʟ]
+ . ɪɴsᴛᴀɢʀᴀᴍ [ᴜʀʟ]
+ . ᴛᴡɪᴛᴛᴇʀ [ᴜʀʟ]
+ . ғᴀᴄᴇʙᴏᴏᴋ [ᴜʀʟ]
+ . ɪɢsᴛᴏʀʏ [ǫᴜᴇʀʏ]
+ . ᴜᴍᴍᴀ [ᴜʀʟ]
+ . ᴍᴇᴅɪᴀғɪʀᴇ [ᴜʀʟ]
+ . ɢɪᴛᴄʟᴏɴᴇ [ᴜʀʟ]
+
+  「 *Text Pro Menu* 」
+ » textmaker glicth [Text]
+ » textmaker glow [Text]
+ » flasmurf [Text]
+ » flarun [Text]
+ » fluming [Text]
+ » flaming [Text]
+ » lava [Text]
+ » neonlight [Text]
+ » fabric [Text]
+ » glue [Text]
+ » luxury [Text]
+ » underwater [Text]
+ » harrypotter [Text]
+ » steel [Text]
+ » metallic [Text]
+ » graffiti [Text]
+ » pencil [Text]
+ » sand [Text]
+ » magma [Text]
+ » joker [Text]
+ » sky [Text]
+ » matrix [Text]
+ » neon [Text]
+ » blackpink [Text]
+ » thunder [Text]
+ » whitebear [Text]
+ » hoorror [Text]
+ » 8bit [Text]
+ » horror [Text]
+ » retro [Text]
+ » pornhub [Text]
+ » juice [Text]
+ » batman [Text]
+ » multicolor [Text]
+ » collwall [Text]
+ » cool [Text]
+ » wonderful [Text]
+ » sketch [Text]
+ » skeleton [Text]
+ » marvel [Text]
+ » foggy [Text]
+ » writing [Text]
+ » halloween [Text]
+ » watercolor [Text]
+ » classic [Text]
+ » rainbow [Text]
+ » sci_fi [Text]
+ » christmas [Text]
+ » ancient [Text]
+ » toxic [Text]
+
+  「 *Stalker Menu* 」
+ » ghstalk [query]
+ » igstalk [query]
+ » ffstalk [query]
+ » supersusstalk [query]
+
+  「 *Internet Menu* 」
+ » ip [query]
+ » iploc [query]
+ » ipinfo [query]
+ » ssweb [query]
+  
+  「 *Search Menu* 」
+ » play [query]
+ » yts [query]
+ » google [query]
+ » gimage [query]
+ » pinterest [query]
+ » wallpaper [query]
+ » wikimedia [query]
+ » ytsearch [query]
+ » ringtone [query]
+ » stalk [option] [query]
+ » webtoons [query]
+ » drakor [query]
+ » anime [query]
+ » character [query]
+ » manga [query]
+
+  「 *Random Menu* 」
+ » coffe
+ » quotesanime
+ » couple
+ » anime
+ » waifu (nsfw)
+ » husbu (nsfw)
+ » neko (nsfw)
+ » shinobu (nsfw)
+ » waifus (nsfw)
+ » nekos (nsfw)
+ » trap (nsfw)
+ » blowjob (nsfw)
+
+  「 *Random Anime Menu* 」
+ » loli
+ » neko
+ » waifu
+ » shinobu
+ » megumin
+ » bully
+ » cuddle
+ » cry
+ » hug
+ » awoo
+ » kiss
+ » lick
+ » pat
+ » smug
+ » bonk
+ » yeet
+ » blush
+ » smile
+ » wave
+ » highfive
+ » handhold
+ » nom
+ » bite
+ » glomp
+ » slap
+ » kill
+ » happy
+ » wink
+ » poke
+ » dance
+ » cringe
+
+  「 *Fun Menu* 」
+ » bagaimanakah
+ » kapankah
+ » apakah
+ » bisakah
+ » rate
+ » wangy
+ » gantengcek
+ » cekganteng
+ » cantikcek
+ » cekcantik
+ » sangecek
+ » ceksange
+ » cekme
+ » gaycek
+ » cekgay
+ » lesbicek
+ » halah
+ » hilih
+ » huluh
+ » heleh
+ » holoh
+ » jadian
+ » jodohku
+ » delttt
+ » tictactoe
+ » family100
+ » tebak [option]
+ » math [mode]
+ » suitpvp [@tag]
+
+  「 *Primbon Menu* 」
+ » nomorhoki
+ » artimimpi
+ » artinama
+ » ramaljodoh
+ » ramaljodohbali
+ » suamiistri
+ » ramalcinta
+ » cocoknama
+ » pasangan
+ » jadiannikah
+ » sifatusaha
+ » rezeki
+ » pekerjaan
+ » nasib
+ » penyakit
+ » tarot
+ » fengshui
+ » haribaik
+ » harisangar
+ » harisial
+ » nagahari
+ » arahrezeki
+ » peruntungan
+ » weton
+ » karakter
+ » keberuntungan
+ » memancing
+ » masasubur
+ » zodiak
+ » shio
+
+  「 *Convert Menu* 」
+ » toimage
+ » removebg
+ » sticker
+ » emojimix
+ » tovideo
+ » togif
+ » tourl
+ » tovn
+ » tomp3
+ » toaudio
+ » ebinary
+ » dbinary
+ » styletext
+ » smeme
+
+  「 *Database Menu* 」
+ » setcmd
+ » listcmd
+ » delcmd
+ » lockcmd
+ » addmsg
+ » listmsg
+ » getmsg
+ » delmsg
+
+  「 *Anonymous Menu* 」
+ » anonymous
+ » start
+ » next
+ » keluar
+ » sendkontak
+
+  「 *Islamic Menu* 」
+ » iqra
+ » hadist
+ » alquran
+ » juzamma
+ » tafsirsurah
+
+  「 *Voice Changer* 」
+ » bass
+ » blown
+ » deep
+ » earrape
+ » fast
+ » fat
+ » nightcore
+ » reverse
+ » robot
+ » slow
+ » tupai
+`
+global.sewanya =
+'  \n *Mau Sewa Bot Ya Kak??* \n *Ketik Aja Langsung Owner* \n *Jangan Lupa Liat Juga List \n  Pembayaran Dibawah ini* 🗿 \n\n*╭━━━━「 *List Sewa Bot* 」*\n┊ ⫹⫺_• Sewa Bot 1 Minggu : 8k_\n┊ ⫹⫺_• Sewa Bot 1 Bulan : 25k_\n┊ ⫹⫺_• Sewa Bot 1 Tahun : 80k_ \n╰═┅═━––––––๑ \n*⫹⫺ PAYMENT:* \n• *Pulsa:* \n[ *083182713104* ] \n• *Gopay:* \n[ *083182713104* ] \n• *Dana:* \n[ *083182423040* ] \n\n–––––– *🐾 Kebijakan* –––––– \n🗣️: Apakah ini RIP kak? \n💬: Kagalah \n▌│█║▌║▌║║▌║▌║█│▌ \n*⫹⫺ Note*: \n*JIKA INGIN MENAMBAHKAN* \n*BOT DI GRUP KALIAN SILAHKAN* \n*KETIK* [ *.owner* ] \n\n*JIKA KALIAN SUDAH MEMBAYAR !!* \n*SILAHKAN KIRIM BUKTI PEMBAYARAN.* \n*KE WA OWNER BOT* \n\n–––––– *🐾SAYONARAA * ––––––' 
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
